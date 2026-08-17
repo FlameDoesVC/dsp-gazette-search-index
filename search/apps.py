477b94ddf9c242e79afb62aeaf37e0400187d291
WovenCoast <wovencoast14782@techie.com>
2026-08-18 00:35:00 +0500

    feat(search): ibay adapter with category priors and quality scoring

--- search/apps.py
+++ search/apps.py
@@ -2,4 +2,12 @@ from django.apps import AppConfig
 
 
 class SearchConfig(AppConfig):
-    name = 'search'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "search"
+
+    def ready(self):
+        from search.adapters import base
+        from search.adapters.ibay import IbayAdapter
+
+        if "ibay" not in base._REGISTRY:
+            base.register(IbayAdapter())
